9a336b1e796bbc28d4501e3e0f9c1bd0f70f3e69
jaxolv <jaxolv@gmail.com>
2022-10-23 12:49:02 -0300

    População de DB com recurso de não repopular

--- src/app/controllers/populateUniversitiesController.js
+++ src/app/controllers/populateUniversitiesController.js
@@ -1,12 +1,23 @@
 const { populateUniversitiesDatabase } = require('../../../script/populateUniversitiesDatabase');
 
-function populateUniversitiesController(req, res) {
-    try {
-        populateUniversitiesDatabase()
+let populated = false
 
-        res.status(201).json({
-            message: "All universities were succesfully merged!"
+async function populateUniversitiesController(req, res) {
+    try {
+        if (!populated) {
+            await populateUniversitiesDatabase()
+
+            populated = true
+
+            return res.status(201).json({
+                message: "All universities were succesfully merged!"
+            })
+        }
+
+        return res.status(200).json( {
+            message: "Universities already merged."
         })
+
     } catch (error) {
         res.status(500).json({ error: error.message })
     }
