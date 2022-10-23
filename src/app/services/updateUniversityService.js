69a2329251b2627cd5d30b3dcbbcd30186bc0c7e
jaxolv <jaxolv@gmail.com>
2022-10-23 17:16:53 -0300

    Validações e recurso de mensagem em erros

--- src/app/services/updateUniversityService.js
+++ src/app/services/updateUniversityService.js
@@ -4,9 +4,17 @@ async function updateUniversityService(id, web_pages, name, domains) {
     const university = { web_pages, name, domains }
 
     try {
-        await University.findByIdAndUpdate({
-            _id: id
-        }, university)
+        if (!web_pages) { return { message: "The field \'web_pages\' has to be informed." } }
+
+        if (!name) { return { message: "The field \'name\' has to be informed." } }
+
+        if (!domains) { return { message: "The field \'domain\' has to be informed." } }
+
+        const universityFounded = await University.findById(id)
+
+        if (!universityFounded) { return { message: "University not found." } }
+
+        await University.updateOne({ _id: id }, university)
 
         return university
     } catch (error) {
