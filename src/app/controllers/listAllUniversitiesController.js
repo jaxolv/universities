56436d84937fd71a17bf8cd086e1b872cf366c1d
jaxolv <jaxolv@gmail.com>
2022-10-24 00:18:18 -0300

    Incluindo listagem por país na listagem geral

--- src/app/controllers/listAllUniversitiesController.js
+++ src/app/controllers/listAllUniversitiesController.js
@@ -1,8 +1,10 @@
 const listAllUniversitiesService = require('../services/listAllUniversitiesService')
 
 async function listAllUniversitiesController(req, res) {
+    const country = req.query.country
+
     try {
-        const universities = await listAllUniversitiesService()
+        const universities = await listAllUniversitiesService(country)
 
         res.status(200).json(universities)
 
--- src/app/services/listAllUniversitiesService.js
+++ src/app/services/listAllUniversitiesService.js
@@ -1,23 +1,40 @@
 const University = require('../models/University')
 
-async function listAllUniversitiesService() {
+async function listAllUniversitiesService(country) {
     try {
-        const universities = await University.find()
+        if (country) {
+            const universities = await University.find({ country })
 
-        const listUniversities = universities.map((university) => {
             return {
-                id: university._id,
-                name: university.name,
-                country: university.country,
-                state_province: university.state_province
+                universities: universities.length,
+                list_universities: universities.map((university) => {
+                    return {
+                        id: university._id,
+                        name: university.name,
+                        country: university.country,
+                        state_province: university.state_province,
+                        for_more_info: `http://universities.hipolabs.com/search?name=${university.name.replace(' ', '%20').replace(' ', '%20').replace(' ', '%20').replace(' ', '%20').replace(' ', '%20').replace(' ', '%20').replace(' ', '%20')}`
+                    }
+                })
             }
-        })
-
-        return {
-            universities: universities.length,
-            list_universities: listUniversities
         }
 
+        if (!country) {
+            const universities = await University.find({ })
+
+            return {
+                universities: universities.length,
+                for_more_info: `http://universities.hipolabs.com/search`,
+                list_universities: universities.map((university) => {
+                    return {
+                        id: university._id,
+                        name: university.name,
+                        country: university.country,
+                        state_province: university.state_province,
+                    }
+                })
+            }
+        }
     } catch (error) {
         console.log(error)
         return { error: error.message }
