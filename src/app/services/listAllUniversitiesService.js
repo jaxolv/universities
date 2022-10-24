5444387538b10db746c3e28a86be58bab6168a82
jaxolv <jaxolv@gmail.com>
2022-10-24 17:38:49 -0300

    Melhoria em repetições de códigos

--- src/app/services/listAllUniversitiesService.js
+++ src/app/services/listAllUniversitiesService.js
@@ -2,15 +2,54 @@ const University = require('../models/University');
 
 async function listAllUniversitiesService(country) {
     try {
-        if (country) {
-            const universities = await University.find({ country });
+        const universitiesByCountry = await University.find({ country });
 
-            if (universities.length === 0) { return { message: "Country not found." } };
+        const universities = await University.find();
+
+        function listUniversities(array) {
+            return array.map((university) => {
+                return {
+                    id: university._id,
+                    name: university.name,
+                    country: university.country,
+                    'state-province': university.state_province,
+                    more_info: `http://universities.hipolabs.com/search?name=${university.name.replace(' ', '%20').replace(' ', '%20').replace(' ', '%20').replace(' ', '%20').replace(' ', '%20').replace(' ', '%20').replace(' ', '%20').replace(' ', '%20').replace(' ', '%20').replace(' ', '%20')}`
+                }
+            })
+        }
+
+        if (country) {
+
+            if (universitiesByCountry.length === 0) { return { message: "Country not found." } };
 
             return {
-                universities: universities.length,
+                universities: universitiesByCountry.length,
                 universities_of_country: `http://universities.hipolabs.com/search?country=${country.toLowerCase()}`,
-                list_universities: universities.map((university) => {
+                list_universities: listUniversities(universitiesByCountry)
+            }
+        };
+
+        return {
+            universities: universities.length,
+            list_universities: listUniversities(universities)
+        }
+    } catch (error) {
+        console.log(error)
+        return { error: error.message }
+    }
+};
+
+module.exports = listAllUniversitiesService;
+
+/* 
+if (country) {
+
+            if (universitiesByCountry.length === 0) { return { message: "Country not found." } };
+
+            return {
+                universities: universitiesByCountry.length,
+                universities_of_country: `http://universities.hipolabs.com/search?country=${country.toLowerCase()}`,
+                list_universities: universitiesByCountry.map((university) => {
                     return {
                         id: university._id,
                         name: university.name,
@@ -23,8 +62,6 @@ async function listAllUniversitiesService(country) {
         };
 
         if (!country) {
-            const universities = await University.find();
-
             return {
                 universities: universities.length,
                 list_universities: universities.map((university) => {
@@ -45,3 +82,4 @@ async function listAllUniversitiesService(country) {
 };
 
 module.exports = listAllUniversitiesService;
+*/
